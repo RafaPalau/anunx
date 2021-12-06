cf85dd0ebcd826a376609bedc23dc68c52d932e6
Rafael <rafaeldonizetip@gmail.com>
2021-12-06 14:26:30 -0300

    Aula 07 Upload de Imagens (parte 2)

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Container,
   Typography,
@@ -8,6 +9,7 @@ import {
   Button,
   IconButton,
 } from "@material-ui/core";
+import { useDropzone } from "react-dropzone";
 import { makeStyles } from "@material-ui/core/styles";
 import { DeleteForever } from "@material-ui/icons";
 import TemplateDefault from "../../src/templates/Default";
@@ -28,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
   thumbsContainer: {
     display: "flex",
     marginTop: 15,
+    flexWrap: "wrap",
   },
   dropzone: {
     width: 200,
@@ -47,6 +50,7 @@ const useStyles = makeStyles((theme) => ({
     width: 200,
     height: 150,
     backgroundSize: "cover",
+    margin: '0 15px 15px 0',
     backgroundPosition: "center center",
 
     "& $mainImage": {
@@ -77,6 +81,22 @@ const useStyles = makeStyles((theme) => ({
 
 const Publish = () => {
   const classes = useStyles();
+  const [files, setFiles] = useState([]);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFile) => {
+      const newFiles = acceptedFile.map((file) => {
+        return Object.assign(file, {
+          preview: URL.createObjectURL(file)
+        });
+      });
+      setFiles([
+        ...files,
+        ...newFiles
+      ]);
+    },
+  });
   return (
     <TemplateDefault>
       <Container maxWidth='sm' className={classes.container}>
@@ -143,28 +163,36 @@ const Publish = () => {
             A primeira imagem é a foto princial do seu anúncio
           </Typography>
           <Box className={classes.thumbsContainer}>
-            <Box className={classes.dropzone}>
+            <Box className={classes.dropzone} {...getRootProps()}>
+              <input {...getInputProps()} />
               <Typography variant='body2' color='textPrimary'>
                 Clique para adicionar ou arraste a imagem para aqui.
               </Typography>
             </Box>
-            <Box
-              className={classes.thumb}
-              style={{
-                backgroundImage: "url(https://source.unsplash.com/random)",
-              }}
-            >
-              <Box className={classes.mainImage}>
-                <Typography variant='body2' color='secondary'>
-                  Principal
-                </Typography>
+
+            {files.map((file, index) => (
+              <Box
+                key={file.name}
+                className={classes.thumb}
+                style={{
+                  backgroundImage: `url(${file.preview})`,
+                }}
+              >
+                {index === 0 ? (
+                  <Box className={classes.mainImage}>
+                    <Typography variant='body2' color='secondary'>
+                      Principal
+                    </Typography>
+                  </Box>
+                ) : null}
+
+                <Box className={classes.mask}>
+                  <IconButton color='secondary'>
+                    <DeleteForever fontSize='large' />
+                  </IconButton>
+                </Box>
               </Box>
-              <Box className={classes.mask}>
-                <IconButton color='secondary'>
-                  <DeleteForever fontSize='large' />
-                </IconButton>
-              </Box>
-            </Box>
+            ))}
           </Box>
         </Box>
       </Container>
