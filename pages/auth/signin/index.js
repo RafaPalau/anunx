5a4ea506c15120ecc4cc3a3e3d37f03df79668df
RafaPalau <rafaeldonizetip@gmai.com>
2022-03-04 18:20:09 -0300

    aula 33 Variável de ambiente - APP_URL

--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -22,7 +22,7 @@ import useToasty from "../../../src/contexts/Toasty";
 import useStyles from "./styles";
 import { Alert } from "@material-ui/lab";
 
-const Signin = () => {
+const Signin = ({ APP_URL }) => {
   const classes = useStyles();
   const router = useRouter();
   const { setToasty } = useToasty();
@@ -32,7 +32,7 @@ const Signin = () => {
 
   const handleGoogleLogin = () => {
     signIn("google", {
-      callbackUrl: "http://localhost:3000/user/dashboard",
+      callbackUrl: `${APP_URL}/user/dashboard`,
     });
   };
 
@@ -40,7 +40,7 @@ const Signin = () => {
     signIn("credentials", {
       email: values.email,
       password: values.password,
-      callbackUrl: "http://localhost:3000/user/dashboard",
+      callbackUrl: `${APP_URL}/user/dashboard`,
     });
   };
 
@@ -159,6 +159,12 @@ const Signin = () => {
       </Container>
     </TemplateDefault>
   );
+};
+
+Signin.getServerSideProps = async function () {
+  return {
+    APP_URL: process.env.APP_URL,
+  };
 };
 
 export default Signin;
